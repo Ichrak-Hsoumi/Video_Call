452ede5490a82d42439605f1e0def89a819c8802
Ichrak-Hsoumi <ash.hsoumi@gmail.com>
2021-06-13 21:08:32 +0100

    first commit

---
+++ Services/IVideoService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using IEvangelist.VideoChat.Models;
+
+namespace IEvangelist.VideoChat.Services
+{
+    public interface IVideoService
+    {
+        /// <summary>
+        /// Gets the Twilio JSON web token so the client can connect to video services.
+        /// </summary>
+        string GetTwilioJwt(string identity);
+
+        /// <summary>
+        /// Gets all of the current active rooms for the app.
+        /// </summary>
+        Task<IEnumerable<RoomDetails>> GetAllRoomsAsync();
+    }
+}
